9e71300142ecd3001083435fbb203aef2b7849f1
HARIOM PATIDAR <149182990+hariompatidar7877@users.noreply.github.com>
2026-03-25 12:02:39 +0530

    Add ExplanationAgent class for trend and risk analysis

---
+++ agents/explanation_agent.py
@@ -0,0 +1,9 @@
+class ExplanationAgent:
+
+    def explain(self, trend, risk):
+        return f"""
+Trend is {trend}.
+Risk level is {risk}.
+Cases increasing due to recent growth trends.
+Suggested monitoring recommended.
+"""
